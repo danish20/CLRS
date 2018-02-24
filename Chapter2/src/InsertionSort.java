6b7ca090f977f954e5a815f2a0e198a174282ac0
Danish Suri <dsuri@ncsu.edu>
2018-02-23 22:19:24 -0500

    Insertion Sort

---
+++ Chapter2/src/InsertionSort.java
@@ -0,0 +1,31 @@
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		int a[] = {5,4,1,2,3,6};
+		a = insertionSort(a);
+		print(a);
+	}
+	
+	public static void print(int a[])
+	{
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]+" ");
+		}
+	}
+	
+	public static int[] insertionSort(int a[])
+	{
+		for(int j=1;j<a.length;j++)
+		{
+			int key = a[j];
+			int i = j-1;
+			while(i>-1 && a[i]>key)
+			{
+				a[i+1] = a[i];
+				i = i-1;
+			}
+			a[i+1] = key;
+		}
+		return a;
+	}
+}
